92c3a25ac97b04d74d6744f7fe4883e858c84bf7
Djordje Knezevic <starwarrior@mycity.rs>
2022-08-17 11:26:02 +0200

    Add logic for sending POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,23 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(meetupData) {};
-  
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-practice3-663c5-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
